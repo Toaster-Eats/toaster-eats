5168bc03a5fab77b1fffa29f4193318a99762416
Baishen Wang <89823629+BaishenWang@users.noreply.github.com>
2024-04-11 21:17:22 -1000

    Create Profile Page

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -17,6 +17,8 @@ import NavBar from '../components/NavBar';
 import SignIn from '../pages/SignIn';
 import NotAuthorized from '../pages/NotAuthorized';
 import LoadingSpinner from '../components/LoadingSpinner';
+import Profile from '../pages/Profile';
+
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => {
@@ -42,6 +44,8 @@ const App = () => {
           <Route path="/admin" element={<AdminProtectedRoute ready={ready}><ListStuffAdmin /></AdminProtectedRoute>} />
           <Route path="/notauthorized" element={<NotAuthorized />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/Profile" element={<Profile />} />
         </Routes>
         <Footer />
       </div>

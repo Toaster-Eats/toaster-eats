67e1fdba6102727a5f4a285f3f4d44b0e4b06f7d
Kenneth de Guzman <156966190+k-deguz@users.noreply.github.com>
2024-04-23 11:29:54 -1000

    Update Mongo.js

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -19,25 +19,28 @@ if (Stuffs.collection.find().count() === 0) {
   }
 }
 
-// Initialize the database with a default data document.
 const addRecipe = (recipe) => {
   console.log(`  Adding Recipe: ${recipe.title} (${recipe.owner})`);
-  Recipes.collection.upsert({ title: recipe.title, owner: recipe.owner }, { $set: recipe });
+  Recipes.collection.insert(recipe);
 };
 
-// Initialize the Recipes collection
-if (Meteor.settings.defaultRecipes) {
-  console.log('Creating default recipes.');
-  Meteor.settings.defaultRecipes.forEach(recipe => addRecipe(recipe));
+// Initialize the Recipes collection if empty.
+if (Recipes.collection.find().count() === 0) {
+  if (Meteor.settings.defaultRecipes) {
+    console.log('Creating default recipes.');
+    Meteor.settings.defaultRecipes.forEach(recipe => addRecipe(recipe));
+  }
 }
 
 const addIngredient = (ingredient) => {
   console.log(`  Adding Ingredient: ${ingredient.name}`);
-  Ingredients.collection.upsert({ name: ingredient.name }, { $set: ingredient });
+  Ingredients.collection.insert(ingredient);
 };
 
 // Initialize the Ingredients collection if empty.
-if (Meteor.settings.defaultIngredients) {
-  console.log('Creating default ingredients.');
-  Meteor.settings.defaultIngredients.forEach(ingredient => addIngredient(ingredient));
+if (Ingredients.collection.find().count() === 0) {
+  if (Meteor.settings.defaultIngredients) {
+    console.log('Creating default ingredients.');
+    Meteor.settings.defaultIngredients.forEach(ingredient => addIngredient(ingredient));
+  }
 }

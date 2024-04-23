298739d59cc423d1f7da5ce037533337844f562f
Kenneth de Guzman <156966190+k-deguz@users.noreply.github.com>
2024-04-23 10:27:11 -1000

    Update Mongo.js to create default ingredients and recipes even when database count is not 0

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -22,26 +22,22 @@ if (Stuffs.collection.find().count() === 0) {
 // Initialize the database with a default data document.
 const addRecipe = (recipe) => {
   console.log(`  Adding Recipe: ${recipe.title} (${recipe.owner})`);
-  Recipes.collection.insert(recipe);
+  Recipes.collection.upsert({ title: recipe.title, owner: recipe.owner }, { $set: recipe });
 };
 
-// Initialize the Recipes collection if empty.
-if (Recipes.collection.find().count() === 0) {
-  if (Meteor.settings.defaultRecipes) {
-    console.log('Creating default recipes.');
-    Meteor.settings.defaultRecipes.forEach(recipe => addRecipe(recipe));
-  }
+// Initialize the Recipes collection
+if (Meteor.settings.defaultRecipes) {
+  console.log('Creating default recipes.');
+  Meteor.settings.defaultRecipes.forEach(recipe => addRecipe(recipe));
 }
 
 const addIngredient = (ingredient) => {
   console.log(`  Adding Ingredient: ${ingredient.name}`);
-  Ingredients.collection.insert(ingredient);
+  Ingredients.collection.upsert({ name: ingredient.name }, { $set: ingredient });
 };
 
 // Initialize the Ingredients collection if empty.
-if (Ingredients.collection.find().count() === 0) {
-  if (Meteor.settings.defaultIngredients) {
-    console.log('Creating default ingredients.');
-    Meteor.settings.defaultIngredients.forEach(ingredient => addIngredient(ingredient));
-  }
+if (Meteor.settings.defaultIngredients) {
+  console.log('Creating default ingredients.');
+  Meteor.settings.defaultIngredients.forEach(ingredient => addIngredient(ingredient));
 }

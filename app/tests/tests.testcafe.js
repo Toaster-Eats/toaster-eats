aade11bc72e10365c5aa643115ed1ded7bb77aba
Kenneth de Guzman <156966190+k-deguz@users.noreply.github.com>
2024-04-25 12:11:32 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -13,44 +13,38 @@ import { navBar } from './navbar.component';
 const credentials = { username: 'john@foo.com', password: 'changeme' };
 
 fixture('meteor-application-template-react localhost test with default db')
-  .page('http://localhost:3000');
+  .page('http://localhost:3000')
+  .beforeEach(async (testController) => {
+    await navBar.gotoSignInPage(testController);
+    await signinPage.signin(testController, credentials.username, credentials.password);
+  });
 
 test('Test: Landing Page Displays', async (testController) => {
   await landingPage.isDisplayed(testController);
 });
 
 test('Test: Sign-in and Sign-out Functions', async (testController) => {
-  await navBar.gotoSignInPage(testController);
-  await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.isLoggedIn(testController, credentials.username);
   await navBar.logout(testController);
   await signoutPage.isDisplayed(testController);
 });
 
 test('Test: Vendor Page Displays', async (testController) => {
-  await navBar.gotoSignInPage(testController);
-  await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.gotoVendorPage(testController);
   await vendorPage.isDisplayed(testController);
 });
 
 test('Test: Ingredients Page Displays', async (testController) => {
-  await navBar.gotoSignInPage(testController);
-  await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.gotoIngredientsPage(testController);
   await ingredientsPage.isDisplayed(testController);
 });
 
 test('Test: Recipes Page is Displayed', async (testController) => {
-  await navBar.gotoSignInPage(testController);
-  await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.gotoRecipesPage(testController);
   await recipesPage.isDisplayed(testController);
 });
 
 test('Test: Add Recipes Page is Displayed', async (testController) => {
-  await navBar.gotoSignInPage(testController);
-  await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.gotoAddRecipesPage(testController);
   await addRecipesPage.isDisplayed(testController);
 });

92fd3455d497332aa35f1a87f2674f8ee80d3d1d
Kenneth de Guzman <156966190+k-deguz@users.noreply.github.com>
2024-04-24 22:10:08 -1000

    Update NavBar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -7,6 +7,7 @@ import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
 const NavBar = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { currentUser } = useTracker(() => ({
     currentUser: Meteor.user() ? Meteor.user().username : '',
   }), []);
@@ -22,14 +23,14 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
-            {currentUser && (
+            {currentUser ? (
               <>
                 <NavDropdown title="Recipes" id="navbar-recipes">
                   <NavDropdown.Item id="list-recipes-nav" as={NavLink} to="/recipes">
                     View All Recipes
                   </NavDropdown.Item>
                   <NavDropdown.Item id="add-recipes-nav" as={NavLink} to="/add-recipe">
-                    Add Recipe
+                    Add a Recipe
                   </NavDropdown.Item>
                 </NavDropdown>
 
@@ -38,37 +39,44 @@ const NavBar = () => {
                     View All Ingredients
                   </NavDropdown.Item>
                   <NavDropdown.Item id="add-ingredients-nav" as={NavLink} to="/add-ingredient">
-                    Add Ingredient
+                    Add an Ingredient
                   </NavDropdown.Item>
                 </NavDropdown>
 
                 <Nav.Link id="vendor-nav" as={NavLink} to="/vendor">Stores Near Campus</Nav.Link>
               </>
-            )}
-            {Meteor.userId() && Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
-              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin">
-                Store Profile
-              </Nav.Link>
-            ) : (
-              <Nav.Link id="your-profile-nav" as={NavLink} to="/your-profile">
-                Your Profile
-              </Nav.Link>
-            )}
+            ) : ''}
+            {/* eslint-disable-next-line no-nested-ternary */}
+            {Meteor.userId() ? (
+              Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
+                <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin"> Store Profile</Nav.Link>
+              ) : (
+                <Nav.Link id="your-profile-nav" as={NavLink} to="/your-profile" key="your-profile">Your Profile</Nav.Link>
+              )
+            ) : null}
+
           </Nav>
           <Nav className="justify-content-end">
             {currentUser === '' ? (
               <NavDropdown id="login-dropdown" title="Login">
                 <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
-                  <PersonFill /> Sign in
+                  <PersonFill />
+                  Sign
+                  in
                 </NavDropdown.Item>
                 <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
-                  <PersonPlusFill /> Sign up
+                  <PersonPlusFill />
+                  Sign
+                  up
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
               <NavDropdown id="navbar-current-user" title={currentUser}>
                 <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
-                  <BoxArrowRight /> Sign out
+                  <BoxArrowRight />
+                  {' '}
+                  Sign
+                  out
                 </NavDropdown.Item>
               </NavDropdown>
             )}

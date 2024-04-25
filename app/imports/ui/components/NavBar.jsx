64e4a2b0c1aada0da17aff8fcc9faf97c2caaeec
Kenneth de Guzman <156966190+k-deguz@users.noreply.github.com>
2024-04-24 20:48:14 -1000

    Update NB (2)

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -6,17 +6,8 @@ import { Roles } from 'meteor/alanning:roles';
 import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
-// Inline style for horizontal alignment
-const inlineStyle = {
-  display: 'flex',
-  alignItems: 'center',
-};
-
-const customNavDropdownStyle = {
-  paddingRight: '5px',
-};
-
 const NavBar = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { currentUser } = useTracker(() => ({
     currentUser: Meteor.user() ? Meteor.user().username : '',
   }), []);
@@ -25,44 +16,32 @@ const NavBar = () => {
     <Navbar bg="light" expand="lg" className="custom-navbar-padding">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          <div style={inlineStyle}>
-            <Image src="/images/logo.png" width="80px" alt="Logo" />
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Image src="/images/logo.png" width="80px" />
           </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
+
             {currentUser ? (
               <>
-                <NavDropdown
-                  className="custom-dropdown"
-                  title="Recipes"
-                  id="recipes-dropdown"
-                  style={customNavDropdownStyle}
-                >
-                  <NavDropdown.Item as={NavLink} to="/recipes">All Our Recipes!</NavDropdown.Item>
-                  <NavDropdown.Item as={NavLink} to="/add-recipe">Add a Recipe!</NavDropdown.Item>
-                </NavDropdown>
-
-                <NavDropdown
-                  className="custom-dropdown"
-                  title="Ingredients"
-                  id="ingredients-dropdown"
-                  style={customNavDropdownStyle}
-                >
-                  <NavDropdown.Item as={NavLink} to="/ingredients">All Available Ingredients!</NavDropdown.Item>
-                  <NavDropdown.Item as={NavLink} to="/add-ingredient">Add an Ingredient!</NavDropdown.Item>
-                </NavDropdown>
-
-                <Nav.Link id="vendor-nav" as={NavLink} to="/vendor">Stores Near Campus</Nav.Link>
+                <Nav.Link id="list-recipes-nav" as={NavLink} to="/recipes" key="recipes">Recipes</Nav.Link>
+                <Nav.Link id="list-ingredients-nav" as={NavLink} to="/ingredients" key="ingredients">Ingredients</Nav.Link>
+                <Nav.Link id="add-recipes-nav" as={NavLink} to="/add-recipe" key="add">Add  Recipe</Nav.Link>
+                <Nav.Link id="add-ingredients-nav" as={NavLink} to="/add-ingredient" key="add">Add Ingredient</Nav.Link>
+                <Nav.Link id="vendor-nav" as={NavLink} to="/vendor" key="vendor">Stores Near Campus</Nav.Link>
               </>
+            ) : ''}
+            {/* eslint-disable-next-line no-nested-ternary */}
+            {Meteor.userId() ? (
+              Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
+                <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin"> Store Profile</Nav.Link>
+              ) : (
+                <Nav.Link id="your-profile-nav" as={NavLink} to="/your-profile" key="your-profile">Your Profile</Nav.Link>
+              )
             ) : null}
 
-            {Meteor.userId() && Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
-              <Nav.Link id="admin-nav" as={NavLink} to="/admin">Store Profile</Nav.Link>
-            ) : (
-              <Nav.Link id="your-profile-nav" as={NavLink} to="/your-profile">Your Profile</Nav.Link>
-            )}
           </Nav>
           <Nav className="justify-content-end">
             {currentUser === '' ? (

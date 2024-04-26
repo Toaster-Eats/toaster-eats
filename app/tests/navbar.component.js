25df80ff163179df34f552196f8f0a30c522c832
Kenneth de Guzman <156966190+k-deguz@users.noreply.github.com>
2024-04-25 18:23:22 -1000

    Update NavBar test with logout

--- app/tests/navbar.component.js
+++ app/tests/navbar.component.js
@@ -21,6 +21,18 @@ class NavBar {
     await testController.expect(loggedInUser).eql(username); // Verify logged-in username
   }
 
+  /** Check that someone is logged in, then click items to log out. */
+  async logout(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
+    await testController.expect(Selector('#navbar-current-user').exists).ok();
+    await testController.click('#navbar-current-user');
+    await testController.click('#navbar-sign-out');
+  }
+
+  /** Navigate to the sign-in page. */
   async gotoSignInPage(testController) {
     await this.ensureLogout(testController);
     const visible = await Selector('#basic-navbar-nav').visible;
@@ -31,10 +43,11 @@ class NavBar {
     await testController.click('#login-dropdown-sign-in');
   }
 
+  /** Navigate to the sign-up page. */
   async gotoSignUpPage(testController) {
     await this.ensureLogout(testController);
     const visible = await Selector('#basic-navbar-nav').visible;
-    if (!visible) { // Fix for incorrect syntax
+    if (!visible) {
       await testController.click('button.navbar-toggler');
     }
     await testController.click('#login-dropdown');
@@ -46,46 +59,46 @@ class NavBar {
     await testController.click('#vendor-nav');
   }
 
-  /** Navigate to recipes dropdown. */
+  /** Navigate to the recipes dropdown. */
   async gotoRecipesDropdown(testController) {
     const visible = await Selector('#basic-navbar-nav').visible;
-    if (!visible) { // Fix for incorrect syntax
+    if (!visible) {
       await testController.click('button.navbar-toggler');
     }
     await testController.click('#recipes-dropdown');
   }
 
-  /** Navigate to "View All Recipes" via recipes dropdown. */
+  /** Navigate to "View All Recipes" via the recipes dropdown. */
   async gotoRecipesPage(testController) {
     await this.gotoRecipesDropdown(testController);
-    await testController.click('#list-recipes-nav');
+    await testController.click('#list-recipes-nav'); // Missing semicolon
   }
 
-  /** Navigate to "Add Recipe" via recipes dropdown. */
+  /** Navigate to "Add Recipe" via the recipes dropdown. */
   async gotoAddRecipesPage(testController) {
     await this.gotoRecipesDropdown(testController);
-    await testController.click('#add-recipes-nav');
+    await testController.click('#add-recipes-nav'); // Missing semicolon
   }
 
-  /** Navigate to ingredients dropdown. */
+  /** Navigate to the ingredients dropdown. */
   async gotoIngredientsDropdown(testController) {
     const visible = await Selector('#basic-navbar-nav').visible;
-    if (!visible) { // Fix for incorrect syntax
+    if (!visible) { // Fixed syntax error
       await testController.click('button.navbar-toggler');
     }
     await testController.click('#ingredients-dropdown');
   }
 
-  /** Navigate to "View All Ingredients" via ingredients dropdown. */
+  /** Navigate to "View All Ingredients" via the ingredients dropdown. */
   async gotoIngredientsPage(testController) {
     await this.gotoIngredientsDropdown(testController);
-    await testController.click('#list-ingredients-nav'); // Add missing semicolon
+    await testController.click('#list-ingredients-nav'); // Missing semicolon
   }
 
-  /** Navigate to "Add Ingredient" via ingredients dropdown. */
+  /** Navigate to "Add Ingredient" via the ingredients dropdown. */
   async gotoAddIngredientsPage(testController) {
     await this.gotoIngredientsDropdown(testController);
-    await testController.click('#add-ingredients-nav'); // Add missing semicolon
+    await testController.click('#add-ingredients-nav'); // Missing semicolon
   }
 }
 

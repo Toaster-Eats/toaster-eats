180543067fdc1f3e82e99a8133609f7bd3171bfd
citycoding <112043863+citycoding@users.noreply.github.com>
2024-04-26 13:37:51 -1000

    Created Footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
+  <footer className="py-3 footer-background">
     <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="https://toaster-eats.github.io/">
-          Our Home Page
-        </a>
-      </Col>
+      <Row>
+        <Col className="text-center">
+          <Image src="/images/logo.png" width="80px" alt="Logo"/>
+          Toaster Eats
+          {' '}
+          <br/>
+          University of Hawaii
+          <br/>
+          Honolulu, HI 96822
+          {' '}
+          <br/>
+          <a href="https://toaster-eats.github.io/">
+            Our Project Page |
+          </a>
+          <a href="https://github.com/Toaster-Eats">
+            &nbsp;Our GitHub
+          </a>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
